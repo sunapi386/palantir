9165ca028509c67546fbcb78bad483dda16c6855
Jason Sun <sunapi386@gmail.com>
2014-07-26 19:34:11 -0700

    rename

--- src/com/palantir/GoodAgg/GoodAgg.java
+++ src/com/palantir/GoodAgg/GoodAgg.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BadAgg {
+public class GoodAgg {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         int mb = 1024 * 1024;
